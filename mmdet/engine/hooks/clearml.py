7088e7528fa8b0800a458e8e0e557694d3a61f85
Assaf Mushinsky <assaf@brodmann17.com>
2023-02-15 12:04:55 +0200

    Implement ClearMLLoggerHook

---
+++ mmdet/engine/hooks/clearml.py
@@ -0,0 +1,29 @@
+from mmengine.dist import master_only
+from mmengine.hooks import Hook
+from mmengine.runner import Runner
+from typing import Optional, Dict
+
+from mmdet.registry import HOOKS
+
+
+@HOOKS.register_module()
+class ClearMLLoggerHook(Hook):
+    def __init__(self, init_kwargs: Optional[Dict] = None) -> None:
+        self.clearml = None
+        self.task = None
+        self.init_kwargs = init_kwargs
+        self.import_clearml()
+
+    def import_clearml(self):
+        try:
+            import clearml
+        except ImportError:
+            raise ImportError('Please run "pip install clearml" to install clearml')
+        self.clearml = clearml
+
+    @master_only
+    def before_run(self, runner) -> None:
+        task_kwargs = self.init_kwargs if self.init_kwargs else {}
+        if 'task_name' not in task_kwargs:
+            task_kwargs['task_name'] = runner.experiment_name
+        self.task = self.clearml.Task.init(**task_kwargs)
