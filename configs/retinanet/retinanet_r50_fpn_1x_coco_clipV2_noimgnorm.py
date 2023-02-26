d505537a9ab34a3da7bf226fe7e7dac73bfcb640
Assaf Mushinsky <assaf@menteebot.com>
2023-02-26 17:02:44 +0200

    .

---
+++ configs/retinanet/retinanet_r50_fpn_1x_coco_clipV2_noimgnorm.py
@@ -0,0 +1,13 @@
+_base_ = './retinanet_r50_fpn_1x_coco.py'
+
+# model settings
+model = dict(
+    bbox_head=dict(
+        retina_cls=dict(
+            type='ClipConvClassPredictor',
+            norm_text=True,
+            norm_image=False,
+            scale_and_bias=True
+        )
+    )
+)
