577778664089741428577b471babb18d6ab34f6f
Assaf Mushinsky <assaf@menteebot.com>
2023-02-27 14:05:40 +0200

    .

---
+++ configs/lvis/retinanet_r50_fpn_1x_coco.py
@@ -0,0 +1,19 @@
+_base_ = [
+    '../_base_/models/retinanet_r50_fpn.py',
+    '../_base_/datasets/lvis_v1_instance.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py',
+    './retinanet_tta.py'
+]
+
+model = dict(
+    bbox_head=dict(num_classes=1203),
+    test_cfg=dict(
+        nms_pre=1000,
+        min_bbox_size=0,
+        score_thr=0.0001,
+        nms=dict(type='nms', iou_threshold=0.5),
+        max_per_img=300))
+
+# optimizer
+optim_wrapper = dict(
+    optimizer=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001))
