ce98998857f1199a2f519e1e950726ca4a8107d3
Assaf Mushinsky <assaf@menteebot.com>
2023-02-27 16:39:44 +0200

    increase r50clip batch size

--- configs/retinanet/retinanet_r50clip_fpn_1x_coco.py
+++ configs/retinanet/retinanet_r50clip_fpn_1x_coco.py
@@ -25,3 +25,5 @@ optim_wrapper = dict(
     type='OptimWrapper',
     optimizer=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001),
     clip_grad=dict(max_norm=35, norm_type=2))
+
+train_dataloader = dict(batch_size=4)
