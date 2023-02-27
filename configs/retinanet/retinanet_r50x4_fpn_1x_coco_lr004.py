300c1e68dd3c21c268756f20e55b60af27c045cb
Assaf Mushinsky <assaf@menteebot.com>
2023-02-27 13:54:12 +0200

    .

--- configs/retinanet/retinanet_r50x4_fpn_1x_coco_lr004.py
+++
@@ -1,6 +0,0 @@
-_base_ = './retinanet_r50x4_fpn_1x_coco.py'
-
-# optimizer
-optim_wrapper = dict(
-    type='OptimWrapper',
-    optimizer=dict(type='SGD', lr=0.04, momentum=0.9, weight_decay=0.0001))
