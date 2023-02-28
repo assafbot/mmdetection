a4565ea452be86e292edcd0d6da1178a74af1924
Assaf Mushinsky <assaf@menteebot.com>
2023-02-28 10:23:43 +0200

    .

--- configs/retinanet/retinanet_r50clip_fpn_1x_coco_frozen_resume.py
+++ configs/retinanet/retinanet_r50clip_fpn_1x_coco_frozen_resume.py
@@ -9,4 +9,4 @@ model = dict(
 )
 
 train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=24, val_interval=1)
-resume = 'mentee://mmdetection/pretrained/retinanet_r50clip_fpn_1x_coco_frozen_epoch_012.pth'
+
