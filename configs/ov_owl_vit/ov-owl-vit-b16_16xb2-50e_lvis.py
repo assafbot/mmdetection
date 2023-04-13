09f4202ee43820e3845eb3c88eaf58644d7cede4
Assaf Mushinsky <assaf@menteebot.com>
2023-04-13 17:50:48 +0300

    .

--- configs/ov_owl_vit/ov-owl-vit-b16_16xb2-50e_lvis.py
+++ configs/ov_owl_vit/ov-owl-vit-b16_16xb2-50e_lvis.py
@@ -7,3 +7,13 @@ model = dict(
     backbone=dict(
         model_name=model_name,
         pretrained=pretrained))
+
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=0,
+    persistent_workers=False)
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=0,
+    persistent_workers=False)
+test_dataloader = val_dataloader
