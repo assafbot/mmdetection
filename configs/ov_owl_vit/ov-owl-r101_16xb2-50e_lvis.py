e6fd1582c50a4098b04b616da4b83511b5beb9b6
Assaf Mushinsky <assaf@menteebot.com>
2023-04-16 13:35:12 +0300

    Revert "."
    This reverts commit e5e88edacd76d90ddf89e33be65040f8c460beea.

--- configs/ov_owl_vit/ov-owl-r101_16xb2-50e_lvis.py
+++ configs/ov_owl_vit/ov-owl-r101_16xb2-50e_lvis.py
@@ -8,3 +8,13 @@ model = dict(
         model_name=model_name,
         pretrained=pretrained,
         init_cfg=dict(type='Pretrained', checkpoint='mentee://mmdetection/pretrained/RN101_openai.pth')))
+
+train_dataloader = dict(
+    batch_size=1,
+    num_workers=0,
+    persistent_workers=False)
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=0,
+    persistent_workers=False)
+test_dataloader = val_dataloader
