2cf697c3fd6cfa5e7c05ce7008359a1a064d3d9f
Assaf Mushinsky <assaf@menteebot.com>
2023-04-20 15:01:32 +0300

    .

---
+++ configs/ov_owl_vit/ov-owl-vit-b32_16xb2-50e_lvis_clip_transformer_1layer.py
@@ -0,0 +1,7 @@
+_base_ = 'ov-owl-vit-b32_16xb2-50e_lvis_clip.py'
+
+model = dict(
+    bbox_head=dict(
+        fc_cls=dict(
+            type='QueryClipTransformerClassPredictor',
+            num_decoder_layers=1)))
